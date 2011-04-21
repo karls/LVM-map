e8e679409594b548ab6ba5672e6351ca9d0fa607
Karl Sutt <karl@sutt.ee>
2011-04-21 14:52:17 +0300

    Bugfix: fin and rus labels were the other way around. Added 'change language' feature for testing

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	session_register("Lang");
-	$Lang = "est";
+	$Lang = isset($_GET["setlang"]) ? $_GET["setlang"] : "est";
 	header('Content-type: text/html; charset=utf-8');
 	$data = file_get_contents("final_data.$Lang.json");
 	
@@ -76,14 +76,14 @@
 			"additional_info" => "Additional info"
 		),
 		"fin" => array(
-			"address" => "Адрес",
-			"price" => "Щена",
-			"additional_info" => "Дополнцтелбная информация"
-		),
-		"rus" => array(
 			"address" => "Lähiosoite",
 			"price" => "Hinta",
 			"additional_info" => "Lisätietoa"
+		),
+		"rus" => array(
+			"address" => "Адрес",
+			"price" => "Щена",
+			"additional_info" => "Дополнцтелбная информация"
 		)
 	);
 	
@@ -156,6 +156,123 @@
 <script type="text/javascript">
 	// The whole of object data
 	var geocode_results = <?php echo $data; ?>;
+	/**
+ * Create the map
+ */
+function initialize() {
+	
+	// Get the object data
+	// Split it into sale and rent data
+	sale_objects = geocode_results[0];
+	rent_objects = geocode_results[1];
+	
+	// Create the options object for the map
+	var myOptions = {
+		zoom: 7,
+		center: new google.maps.LatLng(58.745407, 25.290527),
+		mapTypeId: google.maps.MapTypeId.HYBRID
+	};
+	
+	// Create the map
+	map = new google.maps.Map(
+		document.getElementById("map_canvas"),
+		myOptions
+	);
+	
+	
+	var i;
+	var latlng;
+	var marker;
+	var markerImage;
+	
+	// Infowindow object
+	infowindow = new google.maps.InfoWindow({
+		content: ""
+	});
+	
+	// Loop through the objects
+	for (h = 0; h < geocode_results.length; h++)
+	{
+		for (i = 0; i < geocode_results[h].length; i++)
+		{
+			markerImage = new google.maps.MarkerImage(
+				"markers/" + i + ".png",
+				null,
+				null,
+				new google.maps.Point(0, 23)
+			);
+			
+			
+			for (j = 0; j < geocode_results[h][i].length; j++)
+			{
+				latlng = null;
+				
+				latlng = new google.maps.LatLng(
+					geocode_results[h][i][j][2][0],
+					geocode_results[h][i][j][2][1]
+				);
+				
+				
+				marker = new google.maps.Marker({
+					position: latlng,
+					title: geocode_results[h][i][j][0],
+					map: map,
+					obj_info: "<p class='objinfo'><b><?php echo $infowindow_fields[$Lang]['address']; ?></b>: "
+					+"<a href='http://www.lvm.ee/?op=body&zid="
+					+geocode_results[h][i][j][0]+"&id=63&showLong=1' target='_blank'>"
+					+geocode_results[h][i][j][1].street
+					+" "+geocode_results[h][i][j][1].house_no
+					+", "+geocode_results[h][i][j][1].city
+					+"</a>"
+					+"<br><b><?php echo $infowindow_fields[$Lang]['price']; ?></b>: "
+					+geocode_results[h][i][j][1].price+" €"
+					+"<br><b><?php echo $infowindow_fields[$Lang]['additional_info']; ?></b>: "
+					+geocode_results[h][i][j][1].additional_info
+					+"</p>",
+					icon: markerImage
+				});
+				
+				
+				markers[h][i].push(marker);
+				markers_flattened[h].push(marker);
+				
+				
+				google.maps.event.addListener(marker, 'click', function() {
+					infowindow.setContent(this.obj_info);
+					infowindow.open(map, this);
+				});
+			}// for
+		}// for
+	}// for
+	
+	// Create the MarkerClusterer
+	mc = new MarkerClusterer(map, null, mcOpts);
+	
+	
+	// Create a new calculator -- this one calculates the clusters based on the
+	// visibility of the markers
+	mc.setCalculator(function(markers, numStyles)
+		{
+			var index = 0;
+			var count = 0;
+			for (i = 0; i < markers.length; i++)
+				if (markers[i].getVisible())
+					count++;
+			var dv = count;
+			while (dv !== 0) {
+				dv = parseInt(dv / 10, 10);
+				index++;
+			}
+
+			index = Math.min(index, numStyles);
+			return {
+				text: count,
+				index: index
+			};
+			
+		});
+}// initialize
+
 </script>
 <script type="text/javascript" src="map.js"></script>
 </head>
@@ -232,29 +349,31 @@
 						<td><input type=checkbox name="rentproperty" value="3" onclick="process(this)" checked="checked">
 						<img src="markers/3.png" /><?php echo $obj_type_string_pool[$Lang][3]; ?><br>
 						</td>
-						<td><input type=checkbox name="rentproperty" value="4" onclick="process(this)" checked="checked">
-						<img src="markers/4.png" /><?php echo $obj_type_string_pool[$Lang][4]; ?><br>
-						</td>
-						<td><input type=button name="set" onclick="setAll(document.rentproperties.rentproperty)"
-						       value="<?php echo $obj_type_string_pool[$Lang][10]; ?>"><br>
-						</td>
 						</tr>
 						<tr>
-						<td><input type=checkbox name="rentproperty" value="6" onclick="process(this)" checked="checked">
-						<img src="markers/6.png" /><?php echo $obj_type_string_pool[$Lang][6]; ?><br>
+						<td><input type=checkbox name="rentproperty" value="8" onclick="process(this)" checked="checked">
+						<img src="markers/8.png" /><?php echo $obj_type_string_pool[$Lang][8]; ?><br>
 						</td>
 						<td><input type=checkbox name="rentproperty" value="7" onclick="process(this)" checked="checked">
 						<img src="markers/7.png" /><?php echo $obj_type_string_pool[$Lang][7]; ?><br>
 						</td>
-						<td><input type=checkbox name="rentproperty" value="8" onclick="process(this)" checked="checked">
-						<img src="markers/8.png" /><?php echo $obj_type_string_pool[$Lang][8]; ?><br>
+						<td><input type=checkbox name="rentproperty" value="5" onclick="process(this)" checked="checked">
+						<img src="markers/5.png" /><?php echo $obj_type_string_pool[$Lang][5]; ?><br>
+						</td>
+						<td><input type=checkbox name="rentproperty" value="6" onclick="process(this)" checked="checked">
+						<img src="markers/6.png" /><?php echo $obj_type_string_pool[$Lang][6]; ?><br>
+						</td>
+						</tr>
+						<tr>
+						<td><input type=checkbox name="rentproperty" value="4" onclick="process(this)" checked="checked">
+						<img src="markers/4.png" /><?php echo $obj_type_string_pool[$Lang][4]; ?><br>
 						</td>
 						<td>
 						<input type=checkbox name="rentproperty" value="9" onclick="process(this)" checked="checked">
 						<img src="markers/9.png" /><?php echo $obj_type_string_pool[$Lang][9]; ?><br>
 						</td>
-						<td><input type=checkbox name="rentproperty" value="5" onclick="process(this)" checked="checked">
-						<img src="markers/5.png" /><?php echo $obj_type_string_pool[$Lang][5]; ?><br>
+						<td><input type=button name="set" onclick="setAll(document.rentproperties.rentproperty)"
+						           value="<?php echo $obj_type_string_pool[$Lang][10]; ?>"><br>
 						</td>
 						<td>
 						<input type=button name="clear" onclick="clearAll(document.rentproperties.rentproperty)"
@@ -266,5 +385,9 @@
 	</div>
 	</div>
   <div id="map_canvas" style="width:666px; height:600px"></div>
+	<a href="?setlang=est">est</a>
+	<a href="?setlang=eng">eng</a>
+	<a href="?setlang=fin">fin</a>
+	<a href="?setlang=rus">rus</a>
 </body>
 </html>
